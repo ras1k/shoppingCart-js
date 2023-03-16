5e2fb86fbb28a4dcd6376410316836cd14c4f21c
ras1k <rasikabdullah@gmail.com>
2023-03-17 02:25:31 +0600

    second commit

---
+++ js/case.js
@@ -0,0 +1,34 @@
+function updateNumber(icIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value ;
+    const previouCaseNumber = parseInt(caseNumberString);
+    
+    let newCaseNumber;
+
+    if (icIncrease === true){
+        newCaseNumber = previouCaseNumber + 1;
+    } else {
+        newCaseNumber = previouCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
+function updateCaseTotalPrice (newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateNumber(true);
+    // console.log(newCaseNumber);
+    updateCaseTotalPrice(newCaseNumber);
+});    
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const newCaseNumber = updateNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+    
+})
---
+++ js/phone.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    const newPhoneNumber = previousPhoneNumber + 1;
+    phoneNumberField.value = newPhoneNumber;
+})
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    const newPhoneNumber = previousPhoneNumber - 1;
+    phoneNumberField.value = newPhoneNumber;
+})
